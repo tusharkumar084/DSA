6ebc9c180cd455106c2ef2c82b49d4ca6689393b
Tushar Kumar <tusharkumar8619@gmail.com>
2026-07-15 01:02:39 +0530

    1file added

---
+++ sumnatural.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i, sum , n;
+    cout<<"enter the num:";
+    cin>>n;
+    sum =  0;
+for (int i = 1; i <=n; i++)
+{
+    sum = sum + i;
+}
+
+cout<<sum;
+
+}
