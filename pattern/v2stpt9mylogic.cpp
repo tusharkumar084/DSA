424f02df91943576682f6f132f08c2dc957d21a4
Tushar Kumar <tusharkumar8619@gmail.com>
2026-08-13 22:18:06 +0530

    My first  self logic

---
+++ pattern/v2stpt9mylogic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int row , col;
+    for (int row = 1; row <=5 ; row+=1)
+    {
+        for (int col = 1; col <= row; col+=1)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+}
