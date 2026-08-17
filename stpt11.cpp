1788d916fa07c79c561457d1e6af11719ae98e8f
Tushar Kumar <tusharkumar8619@gmail.com>
2026-08-17 20:20:01 +0530

    no new

---
+++ stpt11.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int row , col;
+    for (int row = 1; row <=5; row+=1)
+    {
+        for (int col = 1; col <=row; col+=1)
+        {
+           cout<<row<<" ";
+           
+        }
+        cout<<endl;
+        
+    }
+    
+}
