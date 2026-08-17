80914efae1a520ffa2c5aa48e27cb0a6f63b831a
Tushar Kumar <tusharkumar8619@gmail.com>
2026-08-17 20:30:54 +0530

    changes

---
+++ pattern/stpt12.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int row , col;
+    for (int row = 1; row <=5; row+=1)
+    {
+        for (int col = row ; col >=1; col-=1)
+        {
+           cout<<col<<" ";
+           
+        }
+        cout<<endl;
+        
+    }
+    
+}
+/*1 
+2 1 
+3 2 1 
+4 3 2 1 
+5 4 3 2 1*/
