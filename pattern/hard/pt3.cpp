f11797148fea718dc3c6eb985274613ecf81c449
Tushar Kumar <tusharkumar8619@gmail.com>
2026-08-28 23:22:09 +0530

    new patterns

---
+++ pattern/hard/pt3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int  row = 1; row <=5; row++)
+    {
+        for (int col = 1; col <=5-row;col++)
+        {
+           cout<<" ";
+
+        }
+        for (int  col = 1; col <=row; col++)
+        {
+            cout<<col;
+        }
+        cout<<endl;
+    }
+    
+}
