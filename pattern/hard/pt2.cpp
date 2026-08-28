6d168ab8f646c58525bfa886d4a2cd5a6dbfea05
Tushar Kumar <tusharkumar8619@gmail.com>
2026-08-28 22:30:18 +0530

    Self done

---
+++ pattern/hard/pt2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int row = 1; row <= 4; row++)
+    {
+        for (int col = 1; col <= 4 - row; col++)
+        {
+            cout << " ";
+        }
+        for (int col = 1; col <= row; col++)
+        {
+            cout << row;
+        }
+        cout << endl;
+    }
+}
