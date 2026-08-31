01c1019962c21454e77905ccdfc41e3bbdcd2afc
Tushar Kumar <tusharkumar8619@gmail.com>
2026-08-31 23:09:39 +0530

    some changes

--- pattern/superhard/pt3ft2self.cpp
+++ pattern/superhard/pt3ft2self.cpp
@@ -1,26 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the num. of rows:";
-    cin>>n;
+    cout << "Enter the num. of rows:";
+    cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        for (int j = i-1; j >=1 ; j--)
+        for (int j = i - 1; j >= 1; j--)
         {
-           cout<<" ";
+            cout << " ";
         }
-        for (int j = 1; j <= 5-(i-1); j++)
+        for (int j = 1; j <= 5 - (i - 1); j++)
         {
-            cout<<"*";
+            cout << "*";
         }
-        for (int  j = n-i; j>= 1; j--)
+        for (int j = n - i; j >= 1; j--)
         {
-           cout<<"*";
+            cout << "*";
         }
-       cout<<endl; 
+        cout << endl;
     }
-    
 }
 /* *********
     *******
