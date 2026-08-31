c56997739c05a009b747493a2da1c5c82e3c166a
Tushar Kumar <tusharkumar8619@gmail.com>
2026-08-31 18:06:37 +0530

    Self made pattern

---
+++ pattern/superhard/pt3ft2self.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the num. of rows:";
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i-1; j >=1 ; j--)
+        {
+           cout<<" ";
+        }
+        for (int j = 1; j <= 5-(i-1); j++)
+        {
+            cout<<"*";
+        }
+        for (int  j = n-i; j>= 1; j--)
+        {
+           cout<<"*";
+        }
+       cout<<endl; 
+    }
+    
+}
+/* *********
+    *******
+     *****
+      ***
+       *   */
