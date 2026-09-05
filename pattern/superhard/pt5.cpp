292d6549e0e867ea845acb978d4d14b5c00f0c92
Tushar Kumar <tusharkumar8619@gmail.com>
2026-09-05 20:05:42 +0530

    SuPer hard Butterfly

---
+++ pattern/superhard/pt5.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter num of rows:";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j <= 2 * n - 2 * i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= n - 1; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j <= 2 * i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
